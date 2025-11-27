e6f0a755b84974540d57752fe0811a1ee5dbdb7b
Hoang Phuong <pl392183@gmail.com>
2025-11-28 02:55:14 +0700

    Update resume.jsx

--- components/resume/resume.jsx
+++ components/resume/resume.jsx
@@ -7,7 +7,7 @@ const Resume = (props) => {
     return (
         <div data-section id='resume' className='mb-16 group'>
             <h2 className='mb-8 font-semibold text-2xl md:text-3xl tracking-wider uppercase opacity-90 transition-all duration-300 group-hover:opacity-100 pb-3 border-b-2 border-transparent group-hover:border-purple-600 group-hover:text-purple-600 transition-colors duration-300'>Resumes</h2>
-            <a1 className='mb-3 font-medium text-sm md:text-base italic '>"Click to view resumes"</a1>
+            <a1 className='mb-3 font-medium text-sm md:text-base italic '>"Click to view my resumes"</a1>
 
             <div className='flex flex-col'>
                 {data.map((item) => (
